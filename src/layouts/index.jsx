75bc10dcc4adc5539f391a3685df7788f586adcd
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2018-04-11 16:40:07 -0500

    Simple layout

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -3,18 +3,45 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import 'tachyons';
 import FaviconHelmet from 'src/layouts/components/FaviconHelmet';
+import sun from 'src/images/sun.svg';
 
 require('prismjs/themes/prism-tomorrow.css');
 
-const Title = styled.h1.attrs({
-  className: 'red',
+const Header = styled.div.attrs({
+  className: 'dib w-100 tc pa3',
+})``;
+
+const HeadingWrapper = styled.div.attrs({
+  className: 'flex justify-between items-center',
+})``;
+
+const Sun = styled.img.attrs({
+  src: sun,
+  className: 'w1 h1',
+})``;
+
+const Heading = styled.div.attrs({
+  className: '',
+})``;
+
+const FlexColumn = styled.div.attrs({
+  className: 'flex flex-column',
 })``;
 
 const BaseLayout = ({ children }) => (
   <div>
     <FaviconHelmet />
-    <Title>Layout Title</Title>
-    {children()}
+    <FlexColumn>
+      <Header>
+        <Sun />
+        <HeadingWrapper>
+          <Heading>Work</Heading>
+          <Heading>About Me</Heading>
+          <Heading>Blog</Heading>
+        </HeadingWrapper>
+      </Header>
+      {children()}
+    </FlexColumn>
   </div>
 );
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Index = () => <div>Index!</div>;
+const HalfBlock = styled.div.attrs({
+  className: 'flex justify-center items-center',
+})`
+  height: 12rem;
+`;
+
+const Index = () => (
+  <div>
+    <HalfBlock>
+      <h2>WORK</h2>
+    </HalfBlock>
+    <HalfBlock>
+      <h4>What I&apos;m working on</h4>
+    </HalfBlock>
+    <HalfBlock>
+      <h2>About Me</h2>
+    </HalfBlock>
+    <HalfBlock>
+      <h4>All about Mong</h4>
+    </HalfBlock>
+    <HalfBlock>
+      <h2>Blog</h2>
+    </HalfBlock>
+    <HalfBlock>
+      <h4>Latest Post: Stuff and Stuff</h4>
+    </HalfBlock>
+  </div>
+);
 
 export default Index;
