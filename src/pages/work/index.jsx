fbe571ec5882f341e6b577a35f787e01c56eb06e
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2018-04-18 16:32:42 -0500

    refactored styles out

--- src/pages/work/index.jsx
+++ src/pages/work/index.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import FadeTransition from 'src/utils/FadeTransition';
 import Link from 'gatsby-link';
-import styled from 'styled-components';
-import {
-  media,
-  transitionEase,
-  gray,
-  grayMedium,
-  grayDark,
-  nearWhite,
-  nearBlack,
-} from 'src/styles';
+import { gray } from 'src/styles';
 import { ContactMe } from 'src/pages/shared';
-import frame from 'src/images/frame.png';
 import toviGif from 'src/images/work/tovi.gif';
 import assurityGif from 'src/images/work/assurity.gif';
 import radialGif from 'src/images/work/radial.gif';
@@ -28,114 +18,22 @@ import {
   tovi,
   yoganonymous,
 } from 'src/logos';
-
-const Container = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 46rem;
-`;
-
-const Block = styled.div`
-  background-color: ${props => props.backgroundColor};
-  padding: 2rem 1rem 8rem 1rem;
-  text-align: center;
-
-  ${media.desktop`
-    padding-top: 8rem;
-    padding-bottom: 8rem;
-  `};
-`;
-
-const Description = styled.h4`
-  line-height: 1.25;
-  margin-bottom: 0.5rem;
-`;
-
-const DescriptionWrapper = styled.div`
-  margin: 1.25rem 0;
-`;
-
-const Brands = styled.div`
-  background-color: ${grayDark};
-  text-align: center;
-`;
-
-const BrandWrapper = styled.div`
-  padding: 0 2rem 0 2rem;
-`;
-
-const ByLine = styled.h5`
-  color: ${grayMedium};
-  padding-top: 1.5rem;
-  margin-bottom: 1.5rem;
-`;
-
-const Brand = styled.img`
-  filter: saturate(0) brightness(10);
-  height: 2rem;
-  margin: 0.5rem;
-`;
-
-const BrandSmall = Brand.extend`
-  height: 1.5rem;
-`;
-
-const InvertBrand = Brand.extend`
-  filter: saturate(0) brightness(0);
-`;
-
-const Button = styled.div`
-  display: inline-block;
-  font-weight: bold;
-  padding: 0.5rem 1.5rem;
-  font-size: 0.75rem;
-  margin: 0.5rem 0 2rem 0;
-  background-color: ${nearWhite};
-  ${transitionEase};
-
-  &:hover {
-    color: ${nearWhite};
-    background-color: ${nearBlack};
-  }
-`;
-
-const InvertButton = Button.extend`
-  background-color: ${nearBlack};
-  color: ${nearWhite};
-
-  &:hover {
-    color: ${nearBlack};
-    background-color: ${nearWhite};
-  }
-`;
-
-const frameWidth = 16;
-
-const Frame = styled.img.attrs({
-  src: frame,
-})`
-  width: ${frameWidth}rem;
-  position: absolute;
-  margin-left: auto;
-  margin-right: auto;
-  left: 0;
-  right: 0;
-  z-index: 2;
-  clip: rect(0px, 1000rem, 8rem, 0px);
-`;
-
-const bezelWidth = 0.5;
-const FrameItem = styled.img`
-  margin-top: ${bezelWidth}rem;
-  width: ${frameWidth - bezelWidth}rem;
-  position: absolute;
-  margin-left: auto;
-  margin-right: auto;
-  left: 0;
-  right: 0;
-  z-index: 1;
-  clip: rect(0px, 1000rem, ${8 - bezelWidth}rem, 0px);
-`;
+import {
+  Container,
+  Block,
+  Description,
+  DescriptionWrapper,
+  Brands,
+  BrandWrapper,
+  ByLine,
+  Brand,
+  BrandSmall,
+  InvertBrand,
+  Button,
+  InvertButton,
+  Frame,
+  FrameItem,
+} from './styles';
 
 export default () => (
   <FadeTransition>
---
+++ src/pages/work/styles.js
@@ -0,0 +1,133 @@
+import styled from 'styled-components';
+import {
+  media,
+  transitionEase,
+  grayMedium,
+  grayDark,
+  nearWhite,
+  nearBlack,
+} from 'src/styles';
+import frame from 'src/images/frame.png';
+
+const baseFrameSize = 8;
+const desktopFrameSize = 14;
+
+const baseFrameWidth = 16;
+const desktopFrameWidth = 25;
+
+const bezelWidth = 0.5;
+
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 46rem;
+`;
+
+export const Block = styled.div`
+  background-color: ${props => props.backgroundColor};
+  padding: 2.5rem 1rem ${baseFrameSize}rem 1rem;
+  text-align: center;
+
+  ${media.desktop`
+    padding-top: ${desktopFrameSize - 4}rem;
+    padding-bottom: ${desktopFrameSize}rem;
+  `};
+`;
+
+export const Description = styled.h4`
+  line-height: 1.25;
+  margin-bottom: 0.5rem;
+`;
+
+export const DescriptionWrapper = styled.div`
+  margin: 1.25rem 0;
+`;
+
+export const Brands = styled.div`
+  background-color: ${grayDark};
+  text-align: center;
+`;
+
+export const BrandWrapper = styled.div`
+  padding: 0 2rem 0 2rem;
+`;
+
+export const ByLine = styled.h5`
+  color: ${grayMedium};
+  padding-top: 3rem;
+  margin-bottom: 1rem;
+`;
+
+export const Brand = styled.img`
+  filter: saturate(0) brightness(10);
+  height: 2rem;
+  margin: 0.5rem;
+`;
+
+export const BrandSmall = Brand.extend`
+  height: 1.5rem;
+`;
+
+export const InvertBrand = Brand.extend`
+  filter: saturate(0) brightness(0);
+`;
+
+export const Button = styled.div`
+  display: inline-block;
+  font-weight: bold;
+  padding: 0.5rem 1.5rem;
+  font-size: 0.75rem;
+  margin: 0.5rem 0 2rem 0;
+  background-color: ${nearWhite};
+  ${transitionEase};
+
+  &:hover {
+    color: ${nearWhite};
+    background-color: ${nearBlack};
+  }
+`;
+
+export const InvertButton = Button.extend`
+  background-color: ${nearBlack};
+  color: ${nearWhite};
+
+  &:hover {
+    color: ${nearBlack};
+    background-color: ${nearWhite};
+  }
+`;
+
+export const Frame = styled.img.attrs({
+  src: frame,
+})`
+  width: ${baseFrameWidth}rem;
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  z-index: 2;
+  clip: rect(0px, 1000rem, ${baseFrameSize}rem, 0px);
+
+  ${media.desktop`
+    width: ${desktopFrameWidth}rem;
+    clip: rect(0px, 1000rem, ${desktopFrameSize}rem, 0px);
+  `};
+`;
+
+export const FrameItem = styled.img`
+  margin-top: ${bezelWidth}rem;
+  width: ${baseFrameWidth - bezelWidth}rem;
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  clip: rect(0px, 1000rem, ${baseFrameSize - bezelWidth}rem, 0px);
+
+  ${media.desktop`
+    width: ${desktopFrameWidth - bezelWidth}rem;
+    clip: rect(0px, 1000rem, ${desktopFrameSize - bezelWidth}rem, 0px);
+  `};
+`;
