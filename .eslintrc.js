a5fdec8b8aeec4deaafbd6e83853816a4c2ff2a2
Mong-Kuen Sun <mongkuen.sun@gmail.com>
2018-04-23 15:01:52 -0500

    eslint webpack loader

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: 'airbnb',
+  plugins: ['prettier'],
+  rules: {
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      { components: ['Link'], specialLink: ['to'] },
+    ],
+    'import/extensions': ['ignorePackages'],
+    'no-debugger': ['warn'],
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: {
+          resolve: {
+            root: __dirname,
+          },
+        },
+      },
+    },
+  },
+};
